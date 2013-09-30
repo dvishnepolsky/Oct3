095ab4f81fcf33b1e00fafe03fe04fbae24fd240
Dimitri Vishnepolsky <dvishnepolsky@mdsol.com>
2013-09-30 11:07:00 -0400

    Initial Commit

---
+++ Oct3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct3
+//
+//  Created by Yan Komizorov on 9/30/13.
+//  Copyright (c) 2013 DV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
